b20d1b523e0443155d64bd75377bd88eed48d81c
matefs <matefs8569@gmail.com>
2024-10-08 07:59:36 -0300

    add: code snippet title no the section

--- src/components/CodeSnippet.tsx
+++ src/components/CodeSnippet.tsx
@@ -25,11 +25,22 @@ export default function CodeSnippet() {
   };
 
   return (
+    <section id="code-snippet" 
+    className="container py-24 sm:py-32 space-y-8">
+    <h2 
+    className="text-3xl lg:text-4xl font-bold md:text-center">
+      Try it{" "}
+      <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+        now !
+      </span>
+    </h2>
+
     <Card>
-      <CardContent>
-        <pre>{code}</pre>
+      <CardContent >
+        <pre className='whitespace-pre-wrap overflow-x-auto'>{code}</pre>
         <Button onClick={handleCopy}>{copied ? 'Copied!' : 'Copy'}</Button>
       </CardContent>
     </Card>
+    </section>
   );
 }
